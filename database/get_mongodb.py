e973a37d037d266b145b010c3841f928ad5f2b80
TekaLishoo <teka.lishoo@gmail.com>
2022-10-14 11:27:16 +0300

    add mongo db

--- database/get_mongodb.py
+++ database/get_mongodb.py
@@ -1,3 +1,8 @@
 from pymongo import MongoClient
+from config.config import Settings
+from fastapi import Depends
 
+
+def get_mongodb(settings=Depends(Settings)):
+    return MongoClient(settings.MONGO_HOST, settings.MONGO_PORT)
 
