71fe6e49bf6899d6e3082d01fd2ea8b4c71c2ca7
TekaLishoo <teka.lishoo@gmail.com>
2022-10-18 11:35:28 +0300

    basic path for dree front

--- dress/routers_dresses/router_dresses.py
+++ dress/routers_dresses/router_dresses.py
@@ -1,15 +1,24 @@
 from fastapi import APIRouter, Depends
+from fastapi.responses import HTMLResponse
+from fastapi.requests import Request
+from fastapi.templating import Jinja2Templates
 from pydantic.validators import List
 from dress.routers_dresses.schemas_dresses import DressDisplay
 from database.get_mongodb import get_mongodb
 from pymongo import MongoClient
-from logging_conf import setup_logger
 
 
 router = APIRouter(prefix='/dresses', tags=['dresses', ])
+templates = Jinja2Templates(directory='dress/templates')
 
 
 @router.get('', response_model=List[DressDisplay])
 def rout_list_dresses(db: MongoClient = Depends(get_mongodb)):
     dresses = db.dresses.objects.find()
     return list(dresses)
+
+
+@router.get('/choice', response_class=HTMLResponse)
+def choice_dress_front(request: Request):
+    return templates.TemplateResponse("dresschoice.html", {'request': request})
+
