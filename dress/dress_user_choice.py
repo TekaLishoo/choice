290f77b927d5b96a8dbce03b422b0775a0cfdb5c
TekaLishoo <teka.lishoo@gmail.com>
2022-10-22 16:26:03 +0300

    subset of dresses for user

---
+++ dress/dress_user_choice.py
@@ -0,0 +1,10 @@
+from random import sample
+from database.get_mongodb import get_mongodb
+
+
+class UserChoice:
+    def __init__(self):
+        db = get_mongodb()
+        total_dresses = db.dresses.objects.count_documents({})
+        self.id_images = sample(range(0, total_dresses), 10)
+        self.cursor = db.dresses.objects.find({'id_image': {'$in': self.id_images}}, {'values': 1, '_id': 0, 'id_image': 1})
--- dress/routers_dresses/router_dresses.py
+++ dress/routers_dresses/router_dresses.py
@@ -3,13 +3,20 @@ from fastapi.responses import HTMLResponse
 from fastapi.requests import Request
 from fastapi.templating import Jinja2Templates
 from pydantic.validators import List
+import re
+import numpy as np
+from PIL import Image
+import base64
+from io import BytesIO
 from dress.routers_dresses.schemas_dresses import DressDisplay
 from database.get_mongodb import get_mongodb
 from pymongo import MongoClient
+from dress.dress_user_choice import UserChoice
 
 
 router = APIRouter(prefix='/dresses', tags=['dresses', ])
 templates = Jinja2Templates(directory='dress/templates')
+user_choice = UserChoice()
 
 
 @router.get('', response_model=List[DressDisplay])
@@ -20,5 +27,16 @@ def rout_list_dresses(db: MongoClient = Depends(get_mongodb)):
 
 @router.get('/choice', response_class=HTMLResponse)
 def choice_dress_front(request: Request):
-    return templates.TemplateResponse("dresschoicechoice.html", {'request': request})
+    print(user_choice.id_images)
+    image_info = next(user_choice.cursor)
+    print(image_info['id_image'])
+    list_str = re.sub("[^0-9]", " ",  image_info['values']).split()
+    array_colors = list(map(int, list_str))
+    np_array_colors = np.array(array_colors).reshape(404, 280, 3)
+    pil_image = Image.fromarray(np_array_colors.astype(np.uint8))
+
+    buff = BytesIO()
+    pil_image.save(buff, format="PNG")
+    img_str = base64.b64encode(buff.getvalue()).decode('ascii')
+    return templates.TemplateResponse("dresschoicechoice.html", {'request': request, 'images': img_str})
 
