f4b6c0cf5b06db0c379e41f918796c1c09164071
TekaLishoo <teka.lishoo@gmail.com>
2022-10-24 13:43:58 +0300

    like-dislike result

--- dress/dress_user_choice.py
+++ dress/dress_user_choice.py
@@ -7,4 +7,10 @@ class UserChoice:
         db = get_mongodb()
         total_dresses = db.dresses.objects.count_documents({})
         self.id_images = sample(range(0, total_dresses), 10)
-        self.cursor = db.dresses.objects.find({'id_image': {'$in': self.id_images}}, {'values': 1, '_id': 0, 'id_image': 1})
+        self.cursor = db.dresses.objects.find(
+            {'id_image': {'$in': self.id_images}},
+            {'values': 1, '_id': 0, 'id_image': 1}
+        )
+        self.result = dict()
+
+
--- dress/routers_dresses/router_dresses.py
+++ dress/routers_dresses/router_dresses.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter, Depends
-from fastapi.responses import HTMLResponse
+from fastapi import APIRouter, Depends, Form
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.requests import Request
 from fastapi.templating import Jinja2Templates
 from pydantic.validators import List
@@ -12,7 +12,6 @@ from dress.routers_dresses.schemas_dresses import DressDisplay
 from database.get_mongodb import get_mongodb
 from pymongo import MongoClient
 from dress.dress_user_choice import UserChoice
-
 
 router = APIRouter(prefix='/dresses', tags=['dresses', ])
 templates = Jinja2Templates(directory='dress/templates')
@@ -28,15 +27,37 @@ def rout_list_dresses(db: MongoClient = Depends(get_mongodb)):
 @router.get('/choice', response_class=HTMLResponse)
 def choice_dress_front(request: Request):
     print(user_choice.id_images)
-    image_info = next(user_choice.cursor)
-    print(image_info['id_image'])
-    list_str = re.sub("[^0-9]", " ",  image_info['values']).split()
-    array_colors = list(map(int, list_str))
-    np_array_colors = np.array(array_colors).reshape(404, 280, 3)
-    pil_image = Image.fromarray(np_array_colors.astype(np.uint8))
+    if user_choice.cursor.alive:
+        image_info = next(user_choice.cursor)
+        print(image_info['id_image'])
+        list_str = re.sub("[^0-9]", " ", image_info['values']).split()
+        array_colors = list(map(int, list_str))
+        np_array_colors = np.array(array_colors).reshape(404, 280, 3)
+        pil_image = Image.fromarray(np_array_colors.astype(np.uint8))
 
-    buff = BytesIO()
-    pil_image.save(buff, format="PNG")
-    img_str = base64.b64encode(buff.getvalue()).decode('ascii')
-    return templates.TemplateResponse("dresschoicechoice.html", {'request': request, 'images': img_str})
+        buff = BytesIO()
+        pil_image.save(buff, format="PNG")
+        img_str = base64.b64encode(buff.getvalue()).decode('ascii')
+        return templates.TemplateResponse("dresschoicechoice.html", {
+            'request': request,
+            'images': img_str,
+            'id_image': image_info['id_image'],
+        })
+    else:
+        return templates.TemplateResponse("choice.html", {
+            'request': request,
+        })
 
+
+@router.post('/choice/like/{id_image}')
+def add_like(request: Request, id_image: str):
+    user_choice.result[id_image] = 1
+    print(user_choice.result)
+    return RedirectResponse(url=request.url_for("choice_dress_front"), status_code=302)
+
+
+@router.post('/choice/dislike/{id_image}')
+def add_like(request: Request, id_image: str):
+    user_choice.result[id_image] = 0
+    print(user_choice.result)
+    return RedirectResponse(url=request.url_for("choice_dress_front"), status_code=302)
